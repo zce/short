168ba1c9cd9c548282c6b542283c70c61b0b518e
zce <w@zce.me>
2021-03-13 22:08:13 +0800

    feat: remove refresh endpoint

--- api/refresh.ts
+++
@@ -1,8 +0,0 @@
-import { VercelRequest, VercelResponse } from '@vercel/node'
-import storage from './storage'
-
-export default async (req: VercelRequest, res: VercelResponse): Promise<any> => {
-  await storage.clearCache()
-  await storage.getUrlBySlug('zce') // recache
-  res.redirect('/')
-}
