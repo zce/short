72e80f8eda57be6385055d86f858d833ae9ad850
zce <w@zce.me>
2021-04-09 15:41:51 +0800

    feat: tcb storage

--- storage/index.ts
+++ storage/index.ts
@@ -3,8 +3,3 @@ import GitHub from './github'
 // import TCB from './tcb'
 
 export default new GitHub()
-
-// import Redis from './redis'
-// import GitHub from './github'
-
-// export default process.env.REDIS_URL != null ? new Redis() : new GitHub()
